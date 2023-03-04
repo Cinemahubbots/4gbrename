1ca3ea6b3a7879101c89e36a3d19de0a6140b9ab
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2023-03-04 21:28:09 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,38 @@
+import asyncio
+from pyrogram import Client, compose,idle
+import os
+
+from main.rename import app as Client2
+
+TOKEN = os.environ.get("BOT_TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+STRING = os.environ.get("STRING", "")
+
+
+bot = Client(
+
+           "Renamer",
+
+           bot_token=TOKEN,
+
+           api_id=API_ID,
+
+           api_hash=API_HASH,
+
+           plugins=dict(root='main'))
+           
+
+if STRING:
+    apps = [Client2,bot]
+    for app in apps:
+        app.start()
+    idle()
+    for app in apps:
+        app.stop()
+    
+else:
+    bot.run()
